568489d03d8bac033b3c62dd5e75ac320218c528
Seth Geyer <seth.geyer@gmail.com>
2013-12-18 12:52:54 -0700

    Created games model and controller and related spec folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuffcookies::Application.config.secret_token = 'b50882820d46772154c15ac489dfd0e4b76aff8eb915e2b858ee607e1e6327be3c7a4adb6e84de1ad82cf4dedb86160fa8e6014f816ffb23ab1d87fcb7c486b4'
